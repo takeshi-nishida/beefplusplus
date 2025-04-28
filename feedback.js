d0fde65a03d965f39360aeba0d1488726bf91844
Takeshi Nishida <takeshi-nishida@users.noreply.github.com>
2025-04-28 12:27:49 +0900

    Add datalist for scores (feedback.js)

--- feedback.js
+++ feedback.js
@@ -4,6 +4,7 @@ chrome.storage.sync.get(optionNames, (items) => {
     if (items["show_submission_text_length"]) showSubmissionTextLength();
     if (items["adjacent_feedback_button"]) duplicateFeedbackButton();
     emphasizeLateSubmission();
+    addDatalistForScore();
 });
 
 function showSubmissionTextLength() {
@@ -34,4 +35,20 @@ function emphasizeLateSubmission() {
             span.classList.add("strong-warning");
         }
     });
+}
+
+function addDatalistForScore() {
+    const maxScore = document.querySelector('input[name="report.maxScore"]').value;    
+    const inputField = document.querySelector('input[name="score"]');
+    
+    const datalist = document.createElement('datalist');
+    datalist.id = 'score-candidates';
+    
+    const option = document.createElement('option');
+    option.value = maxScore;
+    option.textContent = `${maxScore}点（満点）`;
+    datalist.appendChild(option);
+    
+    inputField.parentNode.insertBefore(datalist, inputField.nextSibling);
+    inputField.setAttribute('list', 'score-candidates');
 }
